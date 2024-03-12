a624fdc0c124bb81f7ae0db8ce00172e56a16bfa
Ritul Jain <rituljain07080@gmail.com>
2024-03-12 20:55:59 +0530

    add MySonar file for quality analysis

---
+++ src/main/java/mypkg/MySonar.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class MySonar {
+	public int sum(int a, int b)
+	{
+		int c;
+		int d;
+		c=10+15;
+		return c;
+	}
+	public static void main(String[] args) {
+		MySonar sonar = new MySonar();
+		System.out.println("Sum is :"+sonar.sum(20, 10));
+	}
+}
